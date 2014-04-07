13824f622d6d87334c66a642958b80874208d3e6
FelipePSoares <felipepiressoares_@hotmail.com>
2014-04-07 10:49:17 -0300

    Create a test Project and primary test methods

---
+++ LunchVoterFromHell2.0/LunchVoterFromHellTest/Group.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LunchVoterFromHellTest
+{
+    [TestClass]
+    public class Group
+    {
+        [TestMethod]
+        public void MustCreateGroup()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustCreateGroupAndHaveAOwner()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustEditGroup()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustEditGroupAndHaveAOwner()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustDeleteGroup()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustAddOthersPeopleOnGroup()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustAddOthersPeopleOnGroupAndHaveOnlyOneOwner()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void MustChangeOwner()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
